8fb2d695db240e7a8d9b20ce043ada3bdccc8cc2
Michal Kalinowski <michal.kalinowski@oponeo.pl>
2023-05-19 13:45:07 +0200

    CustomStopWatch

---
+++ CustomStopWatch/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace CustomStopWatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStopWatch sw = new CustomStopWatch();
+            sw.Start();
+            string test = "";
+            for(int i=0; i<10000; i++) 
+            {
+                test += "|test|";
+            }
+            sw.Stop();
+            
+            Console.WriteLine(sw.GetTime().TotalMilliseconds.ToString());
+        }
+
+        class CustomStopWatch
+        {
+            public DateTime StartTime { get; set; }
+
+            public TimeSpan TimeScore { get; set; }
+
+
+            public void Start()
+            {
+                TimeScore = default;
+                StartTime = DateTime.Now;
+            }
+
+            public void Stop()
+            {
+                if (StartTime == default)
+                {
+                    throw new Exception("Time Error");
+                }
+                TimeScore = DateTime.Now - StartTime;
+                StartTime = default;
+            }
+
+            public TimeSpan GetTime()
+            {
+
+                if (TimeScore == default)
+                {
+                    throw new Exception("Time Error");
+                }
+                return TimeScore;
+
+            }
+        }
+    }
+}
