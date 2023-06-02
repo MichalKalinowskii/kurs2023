cb06f495e19acfb2cd698592869ca20a411e6a38
Michal Kalinowski <michal.kalinowski@oponeo.pl>
2023-06-02 15:33:40 +0200

    CustomSingelton

---
+++ CustomSingelton/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CustomSingelton
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+
+	public class Singellton
+	{
+		public static readonly List<object> List = new List<object>();
+		public Singellton(object obj)
+		{
+			List.Add(obj);
+		}
+	}
+
+	
+}
